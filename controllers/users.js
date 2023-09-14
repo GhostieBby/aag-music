89745de0c113ff0fbc456c20b479cb2458446c57
Anthony Fort <ajf2195@columbia.edu>
2023-09-14 21:03:26 +0100

    made start on users.js in controllers

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+import user from '../models/user.js'
+import jwt from 'jsonwebtoken'
+
+export const registerUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    return res.status(201).json({ message: `Welcome ${user.username}` })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const userToLogin = await User.findOne({ email: email })
+    if(!userToLogin) throw new Error ('User not found')
+    if (!userToLogin.validatePassword(password)) throw new Error ('Password invalid')
+    const token = jwt.sign({ sub: userToLogin._id }, process.env.SECRET, { expiresIn: '7d' })
+  } catch (error) {
+    console.log(error)
+  }
+}
