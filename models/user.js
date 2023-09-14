971db769f228ad316862c52ff89901f2d7ba4935
Anthony Fort <ajf2195@columbia.edu>
2023-09-14 20:54:10 +0100

    started coding user.js in models

---
+++ models/user.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+// likeSchema should be in a separate file - likes.js
+const likeSchema = new mongoose.Schema({
+  likedUser: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  likedBy: [{ type: mongoose.Schema.ObjectId, ref: 'User', required: true }]
+})
+
+const userSchema = new mongoose.Schema({
+  username : { type: String, required: true, unique: true, maxlength: 30 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicUrl: { type: String, required: false },
+  soundCloudUrl: { type: String, required: false },
+})
+
+userSchema
+  .virtual('recsAdded', {
+    ref: 'Recommendations',
+    localField: '_id',
+    foreignField: 'addedBy'
+  })
+
+  userSchema
+    .virtual('passwordConfirmation')
+    .set(function(fieldValue){
+      this._passwordConfirmation = fieldValue
+    })
+
+    userSchema
+      .pre('validate', function(next){
+        if (this.isModified('password') && this.password !== this._passwordConfirmation){
+          this.invalidate('passwordConfirmation', 'Passwords do not match.')
+        }
+        next()
+      })
+
+      userSchema
+        .pre('save', function(next){
+          if (this.isModified('password')){
+            this.password = bcrypt.hashSync(this.password, 12)
+          }
+          next()
+        })
+
+        userSchema.methods.validatePassword = function(plainTextPassword){
+          return bcrypt.compareSync(plainTextPassword, this.password)
+        }
+
+        export default mongoose.model('User', userSchema)
