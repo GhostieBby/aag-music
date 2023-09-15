d6cf2568200270af6a13426f5b2b5bcb8f3a23e0
Anthony Fort <ajf2195@columbia.edu>
2023-09-15 15:49:09 +0100

    added dummy users to the database

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -13,7 +13,6 @@ export const addReview = async (req, res) => {
     if (req.user._id != id){ // the equality operator has one equals sign - check this
       user.reviews.push({ ...req.body, addedB: req.user._id })
     }
-    
     await user.save()
     return res.status(201).json(user)
   } catch (error) {
@@ -24,26 +23,26 @@ export const addReview = async (req, res) => {
 
 // 
 
-// * Create Review
-// post /recs/:id/reviews
-export const createReview = async (req, res) => {
-  const { id } = req.params
-  if (!mongoose.isValidObjectId(id)) {
-    return res.status(422).json({ error: 'Invalid ObjectID' })
-  }
-  try {
-    const rec = await Rec.findById(id)
-    if (!rec) {
-      throw new Error('Rec not found')
-    }
-    rec.reviews.push({ ...req.body, addedBy: req.user._id })
-    await rec.save()
-    return res.status(201).json(rec)
-  } catch (error) {
-    console.log(error)
-    return res.status(404).json({ error: error.message })
-  }
-}
+// // * Create Review
+// // post /recs/:id/reviews
+// export const createReview = async (req, res) => {
+//   const { id } = req.params
+//   if (!mongoose.isValidObjectId(id)) {
+//     return res.status(422).json({ error: 'Invalid ObjectID' })
+//   }
+//   try {
+//     const rec = await Rec.findById(id)
+//     if (!rec) {
+//       throw new Error('Rec not found')
+//     }
+//     rec.reviews.push({ ...req.body, addedBy: req.user._id })
+//     await rec.save()
+//     return res.status(201).json(rec)
+//   } catch (error) {
+//     console.log(error)
+//     return res.status(404).json({ error: error.message })
+//   }
+// }
 
 export const deleteReview = async (req, res) => {
   const { recId, reviewId }
---
+++ db/data/users.js
@@ -0,0 +1,128 @@
+export default [
+  {
+    username: 'admin',
+    email: 'admin@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    likes: 0,
+  },
+  {
+    username: 'jill',
+    email: 'jill@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    likes: 0,
+  },
+
+  {
+    username: 'bob',
+    email: 'bob@email.com',
+    password: 'password123',
+    passwordConfirmation: 'password123',
+    likes: 0,
+  },
+
+  {
+    username: 'alice',
+    email: 'alice@email.com',
+    password: 'secret',
+    passwordConfirmation: 'secret',
+    likes: 0,
+  },
+
+  {
+    username: 'john',
+    email: 'john@email.com',
+    password: 'mysecurepassword',
+    passwordConfirmation: 'mysecurepassword',
+    likes: 0,
+  },
+
+  {
+    username: 'sarah',
+    email: 'sarah@email.com',
+    password: 'sarah123',
+    passwordConfirmation: 'sarah123',
+    likes: 0,
+  },
+
+  {
+    username: 'david',
+    email: 'david@email.com',
+    password: 'davidpass',
+    passwordConfirmation: 'davidpass',
+    likes: 0,
+  },
+
+  {
+    username: 'emily',
+    email: 'emily@email.com',
+    password: 'emilypassword',
+    passwordConfirmation: 'emilypassword',
+    likes: 0,
+  },
+
+  {
+    username: 'michael',
+    email: 'michael@email.com',
+    password: 'mike123',
+    passwordConfirmation: 'mike123',
+    likes: 0,
+  },
+
+  {
+    username: 'lisa',
+    email: 'lisa@email.com',
+    password: 'lisapass',
+    passwordConfirmation: 'lisapass',
+    likes: 0,
+  },
+
+  {
+    username: 'kevin',
+    email: 'kevin@email.com',
+    password: 'kevinpass',
+    passwordConfirmation: 'kevinpass',
+    likes: 0,
+  },
+
+  {
+    username: 'hannah',
+    email: 'hannah@email.com',
+    password: 'hannah123',
+    passwordConfirmation: 'hannah123',
+    likes: 0,
+  },
+
+  {
+    username: 'peter',
+    email: 'peter@email.com',
+    password: 'peterpass',
+    passwordConfirmation: 'peterpass',
+    likes: 0,
+  },
+
+  {
+    username: 'olivia',
+    email: 'olivia@email.com',
+    password: 'oliviapassword',
+    passwordConfirmation: 'oliviapassword',
+    likes: 0,
+  },
+
+  {
+    username: 'william',
+    email: 'william@email.com',
+    password: 'williampass',
+    passwordConfirmation: 'williampass',
+    likes: 0,
+  },
+
+  {
+    username: 'natalie',
+    email: 'natalie@email.com',
+    password: 'natalie123',
+    passwordConfirmation: 'natalie123',
+    likes: 0,
+  }
+]
